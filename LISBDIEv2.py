655c2e5af562cd1477ddc78f5bf6ec4d44066904
pendragons-code <88629349+pendragons-code@users.noreply.github.com>
2022-05-05 00:26:48 +0800

    Update LISBDIEv2.py
    resolved issue and found alternatives to the enter thing... also, here is a inheritance of v1's capabilities.

--- LISBDIEv2.py
+++ LISBDIEv2.py
@@ -2,20 +2,25 @@ import pyautogui
 import time
 import math
 pyautogui.FAILSAFE = False
-msg = input("Have you copied the item yet? [y/n]")
-if msg != "y" and msg != "n" or msg == "no":
-    print("Copy the item and run LISBDIE V2 again!")
-n = input("How many times?: ")
-c = input("Countdown?: ")
-d =input("Delay per message?: ")
 met = input("""
 [1]: Copy paste method (no formatting binds).
 [2]: Copy paste method (with formatting binds).
-[3]: Middle click method
+[3]: Middle click method.
+[4]: Typewrite method.
 
 
 Pls select a method of input: """)
 metter = int(met)
+if metter == 1 or metter == 2 or metter == 3:
+    msg = input("Have you copied the item yet? [y/n]")
+    if msg != "y" and msg != "n" or msg == "no":
+        print("Copy the item and run LISBDIE V2 again!")
+
+if metter == 4:
+    msg = input("Enter the message: ")
+n = input("How many times?: ")
+c = input("Countdown?: ")
+d =input("Delay per message?: ")
 count = int(c)
 delay = int(d)
 if math.isnan(delay) or math.isnan(count) or math.isnan(metter):
@@ -35,19 +40,22 @@ if metter == 1:
     for i in range(0, int(n)):
         time.sleep(delay)
         pyautogui.hotkey('ctrl', 'v')
-        time.sleep(0.01)
-        pyautogui.hotkey('enter')
+        pyautogui.typewrite('\n')
 
 if metter == 2:
     for i in range(0, int(n)):
         time.sleep(delay)
         pyautogui.hotkey('ctrl', 'shift', 'v')
-        time.sleep(0.01)
-        pyautogui.hotkey('enter')
+        pyautogui.typewrite('\n')
 
 if metter == 3:
     for i in range(0, int(n)):
         time.sleep(delay)
         pyautogui.click(button='middle')
-        time.sleep(0.01)
-        pyautogui.hotkey('enter')
+        pyautogui.typewrite('\n')
+
+if metter == 4:
+    for i in range(0, int(n)):
+        time.sleep(delay)
+        pyautogui.typewrite(msg)
+        pyautogui.typewrite('\n')
