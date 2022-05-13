3ec1ea24bdfc9c1d5d2c0ff0e5734f96ed36dd42
pendragons-code <88629349+pendragons-code@users.noreply.github.com>
2022-05-13 19:46:37 +0800

    Update and rename LISBDIEv2.py to LISBDIEv3.py
    I HAVE NO CLUE WHY, EVERY SINGLE TIME I WANT TO WORK ON THIS THING I AM OUTSIDE SOMEWHERE AND DOING THIS FROM MY PHONE BECAUSE WHEN I HAVE MY PC I FORGET THIS EXISTS.

--- LISBDIEv3.py
+++ LISBDIEv3.py
@@ -6,8 +6,8 @@ met = input("""
 [1]: Copy paste method (no formatting binds).
 [2]: Copy paste method (with formatting binds).
 [3]: Middle click method.
-[4]: Typewrite method.
-
+[4]: Typewrite method. (command line entry)
+[5]: Typewrite from txt file.
 
 Pls select a method of input: """)
 metter = int(met)
@@ -18,13 +18,15 @@ if metter == 1 or metter == 2 or metter == 3:
 
 if metter == 4:
     msg = input("Enter the message: ")
+if metter == 5:
+    source = input("Directory? (must be a txt file!) :")
 n = input("How many times?: ")
 c = input("Countdown?: ")
 d =input("Delay per message?: ")
 count = int(c)
 delay = int(d)
-if math.isnan(delay) or math.isnan(count) or math.isnan(metter):
-    print("You did not enter numericals for the variables I needed! Run LISBDIE V2 again!")
+if math.isnan(delay) or math.isnan(count) or math.isnan(metter) or delay < 0 or count < 0:
+    print("You did not enter numericals for the variables I needed! Run LISBDIE V3 again!")
 
 while(count != 0):
     print(count)
@@ -32,10 +34,13 @@ while(count != 0):
     count -= 1
 
 print("""
-_    ___ ___ ___ ___ ___ _____   _____ 
-| |  |_ _/ __| _ )   \_ _| __\ \ / /_  )
-| |__ | |\__ \ _ \ |) | || _| \ V / / / 
-|____|___|___/___/___/___|___| \_/ /___|""")
+.____    .___  ___________________________  .__________________   ____________  
+|    |   |   |/   _____/\______   \______ \ |   \_   _____/\   \ /   /\_____  \ 
+|    |   |   |\_____  \  |    |  _/|    |  \|   ||    __)_  \   Y   /   _(__  < 
+|    |___|   |/        \ |    |   \|    `   \   ||        \  \     /   /       \
+|_______ \___/_______  / |______  /_______  /___/_______  /   \___/   /______  /
+        \/           \/         \/        \/            \/                   \/ 
+        """)
 if metter == 1:
     for i in range(0, int(n)):
         time.sleep(delay)
@@ -59,3 +64,13 @@ if metter == 4:
         time.sleep(delay)
         pyautogui.typewrite(msg)
         pyautogui.typewrite('\n')
+if metter == 5:
+    for i in range(0, int(n)):
+        try:
+            f = open(source,'r')
+            for word in f:
+                time.sleep(delay)
+                pyautogui.typewrite(word)
+                pyautogui.typewrite("\n")
+        except:
+            print("The directory you stated might not exist!")
